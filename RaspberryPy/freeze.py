d6a61723c4298896fc22bfa844986769d4611571
Naoya Sugita <g1531072@m.sojo-u.ac.jp>
2017-10-29 03:47:28 +0900

    add freeze.py

---
+++ RaspberryPy/freeze.py
@@ -0,0 +1,23 @@
+import requests
+import os
+import zipfile
+
+def download_file(url):
+    file = url.split('/')[-1]
+    r = requests.get(url, stream=True)
+    with open(file, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                f.flush()
+        return filename
+    return False
+
+def unzip(file_name):
+    with zipfile.ZipFile( file_name ,'r') as f:
+        f.extractall("./plugins/")
+
+#example_url
+url = 'http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip'
+filename = download_file(url)
+unzip(filename)
