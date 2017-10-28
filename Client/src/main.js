9db5dbf390df1c326e2077a32cfb0fe13e273ae0
Marei Kikukawa <contact@calmery.me>
2017-10-28 09:52:03 +0900

    Add express

--- Client/src/main.js
+++ Client/src/main.js
@@ -1,1 +1,12 @@
-// Dummy
+import path from 'path'
+import express from 'express'
+
+const app = express()
+
+path.resolve( __dirname, 'public' )
+  |> express.static
+  |> app.use
+
+const port = app.listen( () => {
+  console.log( 'Running app on localhost:' + port )
+} ).address().port
