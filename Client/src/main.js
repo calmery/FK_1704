3ffeed542ee3cf405a8f7b13a8fe040f9abf863e
Marei Kikukawa <contact@calmery.me>
2017-10-28 10:08:09 +0900

    Add socket.io

--- Client/src/main.js
+++ Client/src/main.js
@@ -1,5 +1,6 @@
 import path from 'path'
 import express from 'express'
+import socketIo from 'socket.io'
 
 const app = express()
 
@@ -7,6 +8,19 @@ path.resolve( __dirname, 'public' )
   |> express.static
   |> app.use
 
-const port = app.listen( () => {
-  console.log( 'Running app on localhost:' + port )
-} ).address().port
+const server = app.listen( () => {
+  console.log( 'Running app on localhost:' + server.address().port )
+} )
+
+const port = server.address().port
+
+const io = socketIo( server )
+
+io.sockets.on( 'connection', socket => {
+  console.log( 'Connected' )
+
+  socket.on( 'message', message => {
+    console.log( new Date(), message )
+    socket.emit( 'message', 'Recieved ' + new Date() )
+  } )
+} )
