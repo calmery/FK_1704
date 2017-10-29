f2300a27bb3cc38e957cce78a1736432b5f44535
peperoncino000 <hm.pudding0715@gmail.com>
2017-10-29 13:30:28 +0900

    create sensor learning model

--- python/sensor_rnn.py
+++ python/sensor_rnn.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, GRU, Activation
+from keras.layers import LSTM, Dense, Dropout
 import numpy as np
 from keras.utils import np_utils
 
@@ -11,8 +11,6 @@ def main():
     model.add(LSTM(32, return_sequences=True,
                 input_shape=(3000, 400)))
     model.add(LSTM(32, return_sequences=True)) 
-    # model.add(LSTM(32)) 
-    # model.add(Dense(10, activation='softmax'))
     model.add(LSTM(32, return_sequences=True)) 
     model.add(LSTM(10, activation='softmax')) 
 
@@ -34,21 +32,19 @@ def test():
     print(x_train.shape)
 
     model = Sequential()
-    model.add(LSTM(32, return_sequences=True,
-                input_shape=(150, 140)))
-    model.add(LSTM(64, return_sequences=True)) 
-    model.add(LSTM(32)) 
+    model.add(LSTM(256, return_sequences=True,
+                input_shape=(140, 3)))
+    model.add(LSTM(256, return_sequences=True)) 
+    model.add(LSTM(256, return_sequences=True)) 
+    model.add(LSTM(256)) 
     model.add(Dense(10, activation='softmax'))
-    # model.add(LSTM(32, return_sequences=True)) 
-    # model.add(LSTM(10, activation='softmax')) 
-
 
     model.compile(loss='categorical_crossentropy',
                 optimizer='Adam',
                 metrics=['accuracy'])
 
     model.fit(x_train, y_train,
-            batch_size=25, epochs=30)
+            batch_size=10, epochs=200)
 
     model.save('specal_pen_test_model.h5')
 
