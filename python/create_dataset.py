fb400aa8b836129655a8e00c03bbec48ca34e84b
peperoncino000 <hm.pudding0715@gmail.com>
2017-10-29 10:36:20 +0900

    fix

--- python/create_dataset.py
+++ python/create_dataset.py
@@ -1,3 +1,89 @@
+# # import numpy as np
+# # from PIL import Image
+# # import os
+
+# # image_dataset = []
+# # label_dataset = []
+
+
+# # for index, label in enumerate(os.listdir('dataset')):
+# #     if label == '.DS_Store':
+# #         continue
+
+# #     dataset_dir = 'dataset/' + label
+
+# #     print(label)
+
+# #     for number_data in os.listdir(dataset_dir):
+# #         if number_data != '.DS_Store':
+# #             filepath = dataset_dir + '/' + number_data
+# #             image = np.array(Image.open(filepath).resize((32, 32)))
+# #             if image.size != 3072:
+# #                 image_dataset.append(image.astype('float32') / 255.)
+# #                 label_dataset.append(index)
+
+# # np.save('hiragana_labels.npy', np.array(label_dataset))
+# # np.save('hiragana_dataset.npy', np.array(image_dataset))
+
+# import numpy as np
+# from PIL import Image
+# import os
+# import matplotlib.pyplot as plt
+
+# label_index = {
+#     'zero' : 0,
+#     'one' : 1,
+#     'two' : 2,
+#     'three' : 3,
+#     'four' : 4,
+#     'five' : 5,
+#     'six' : 6,
+#     'seven' : 7,
+#     'eight' : 8,
+#     'nine' : 9,
+# }
+
+# timesteps = 400
+
+# # result = np.array([])
+# result = []
+# label_dataset = []
+
+# arr = []
+
+# for index, label in enumerate(os.listdir('dataset100')):
+   
+#     if label != '.DS_Store':
+#         data = np.load('dataset100/' + label)
+#         number = label[0:-4]
+#         # print(data)
+#         # break
+#         for d in data:
+#             # print(len(d[0]))
+#             # print(timesteps-len(d[0]))
+#             print(d.shape)
+#             for i in d:
+#                 for j in range(timesteps-len(d[0])):
+#                     i = np.append(i, 0.0)
+#                 arr.append(i)
+#             # print(arr)
+#             # d = np.c_[d, np.zeros([3, timesteps-len(d[0])])]
+#             # d = np.column_stack((d, np.zeros((3, timesteps-len(d[0])))))
+#             # print(d)
+#             # break
+
+#             # result = np.append(result,d)
+#             result.append(arr)
+#             label_dataset.append(label_index[number])
+#             print(label)
+#         # break
+
+# np.save("./numbers.npy", result)
+# np.save("./label.npy", np.array(label_dataset))
+
+
+
+
 # import numpy as np
 # from PIL import Image
 # import os
@@ -43,7 +129,7 @@ label_index = {
     'nine' : 9,
 }
 
-timesteps = 140
+timesteps = 400
 
 # result = np.array([])
 result = []
@@ -55,20 +141,21 @@ for index, label in enumerate(os.listdir('dataset')):
    
     if label != '.DS_Store':
         data = np.load('dataset/' + label)
+
         number = label[0:-4]
         # print(data)
         # break
         for d in data:
-            print(len(d[0]))
-            print(timesteps-len(d[0]))
+            # print(len(d[0]))
+            # print(timesteps-len(d[0]))
             for i in d:
                 for j in range(timesteps-len(d[0])):
-                    i = np.append(i, 0.0)
+                    i = np.append(i, [0.0, 0.0, 0.0])
                 arr.append(i)
-            print(arr)
+            # print(arr)
             # d = np.c_[d, np.zeros([3, timesteps-len(d[0])])]
             # d = np.column_stack((d, np.zeros((3, timesteps-len(d[0])))))
-            print(d)
+            # print(d)
             # break
 
             # result = np.append(result,d)
@@ -77,6 +164,6 @@ for index, label in enumerate(os.listdir('dataset')):
             print(label)
         # break
 
-np.save("./numbers.npy", result)
-np.save("./label.npy", np.array(label_dataset))
+np.save("./numbers_test.npy", result)
+np.save("./label_test.npy", np.array(label_dataset))
 
--- python/sensor_rnn.py
+++ python/sensor_rnn.py
@@ -1,25 +1,57 @@
 from keras.models import Sequential
-from keras.layers import LSTM, Dense
+from keras.layers import LSTM, Dense, GRU, Activation
 import numpy as np
+from keras.utils import np_utils
 
-data_dim = 3
-timesteps = 140
-num_classes = 10
+def main():
+    x_train = np.load('numbers.npy')
+    y_train = np_utils.to_categorical(np.load('label.npy'), num_classes=10)
 
-x_train = np.load('numbers.npy')
-y_train = np.load('label.npy')
+    model = Sequential()
+    model.add(LSTM(32, return_sequences=True,
+                input_shape=(3000, 400)))
+    model.add(LSTM(32, return_sequences=True)) 
+    # model.add(LSTM(32)) 
+    # model.add(Dense(10, activation='softmax'))
+    model.add(LSTM(32, return_sequences=True)) 
+    model.add(LSTM(10, activation='softmax')) 
 
-print(x_train.shape)
-model = Sequential()
-model.add(LSTM(30, return_sequences=True,
-               input_shape=(None, timesteps, 3)))
-model.add(LSTM(30, return_sequences=True)) 
-model.add(LSTM(30)) 
-model.add(Dense(10, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy',
+                optimizer='Adam',
+                metrics=['accuracy'])
 
-model.fit(x_train, y_train,
-          batch_size=2, epochs=20)
+    model.fit(x_train, y_train,
+            batch_size=32, epochs=20)
+
+    model.save('specal_pen_model.h5')
+
+
+def test():
+    x_train = np.load('numbers_test.npy')
+    y_train = np_utils.to_categorical(np.load('label_test.npy'), num_classes=10)
+
+    print(x_train.shape)
+
+    model = Sequential()
+    model.add(LSTM(32, return_sequences=True,
+                input_shape=(150, 140)))
+    model.add(LSTM(64, return_sequences=True)) 
+    model.add(LSTM(32)) 
+    model.add(Dense(10, activation='softmax'))
+    # model.add(LSTM(32, return_sequences=True)) 
+    # model.add(LSTM(10, activation='softmax')) 
+
+
+    model.compile(loss='categorical_crossentropy',
+                optimizer='Adam',
+                metrics=['accuracy'])
+
+    model.fit(x_train, y_train,
+            batch_size=25, epochs=30)
+
+    model.save('specal_pen_test_model.h5')
+
+if __name__ == '__main__':
+    test()
+    # main()
