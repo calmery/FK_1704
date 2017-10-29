fc9305c9bff75613a932785f40f9ba4625557235
Naoya Sugita <g1531072@m.sojo-u.ac.jp>
2017-10-29 10:30:30 +0900

    fix get_acc.py

--- python/get_acc.py
+++ python/get_acc.py
@@ -27,33 +27,33 @@ def main(num):
     try:
         while True:
             data = bus.read_i2c_block_data(address, 0x3B ,6)
-            
+
             x_data = (2.0 / float(0x8000)) * (data[0] << 8 | data[1])
             y_data = (2.0 / float(0x8000)) * (data[2] << 8 | data[3])
             z_data = (2.0 / float(0x8000)) * (data[4] << 8 | data[5])
-            
+
             x.append(x_data)
             y.append(y_data)
             z.append(z_data)
 
             print ("X:%+8.7f" %x_data + " Y:%+8.7f" %y_data + " Z:%8.7f" %z_data)
-            
+
             time.sleep(0.01)
     except KeyboardInterrupt:
         print("================================================")
-        print("=                    "+str(count + 1)  +"                  = ")        
+        print("=                    "+str(count + 1)  +"                  = ")
         print("==================================================")
         time.sleep(0.3)
         count += 1
-
-        arr.append([x,y,z])
+        arr.append([x],[y],[z])
         x,y,z = [],[],[]
+
         f = open("test.txt","w")
         f.write(str(arr))
-        
+
         if count < num:
             main(sample_num)
-        
+
         if count == 50:
             f.close()
 
@@ -71,9 +71,5 @@ data = convert(arr)
 def make_dataset(num,variety):
     for i in range(num):
         np.save('./dataset/' + variety + '.npy', data)
-
-#def make_arr(arr):
-#    with open('data.txt', 'w') as f:
-#        f.write(arr)
 
 make_dataset(sample_num,name)
